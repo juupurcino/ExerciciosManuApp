d59afefbfd69877cf4369aac5121b10dba790bf3
juupurcino <jdiaspurcino@gmail.com>
2024-09-13 16:31:43 -0300

    Exercicio 13 pronto

---
+++ Exercício13.py
@@ -0,0 +1,9 @@
+print("_____________________________________________________\n")
+print("SISTEMA DE TABUADA \n")
+print("_____________________________________________________\n")
+num = int(input("digite o número da tabuada que gostaria de consultar: \n"))
+
+
+for i in range(11):
+    print(f"{num} X {i} = {num*i}")
+

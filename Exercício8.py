6cc66472137adecbb067bc47c7f8b5d1d88838ef
juupurcino <jdiaspurcino@gmail.com>
2024-09-13 15:59:45 -0300

    Exercicio 9 pronto

--- Exercício8.py
+++ Exercício8.py
@@ -4,10 +4,11 @@ achou = 0
 for i in range(10):
     valor = int(input(f"Informe o {i+1}º valor: "))
     list.append(valor)
+
 busca = int(input("\n Informe um valor pra ser procurado: "))
 
-for i in list:
-    if(busca == list[i]):
+for j in list:
+    if(busca == list[j]):
         achou = achou + 1
 
 print(f"O número foi achado {achou} vezes.")
---
+++ Exercício9.py
@@ -0,0 +1,9 @@
+list = []
+
+for i in range(5):
+    num = int(input("\nDigite: "))
+    list.append(num)
+
+for j in list:
+    print(f"Valor indice [{j+1}]: {j}")
+	

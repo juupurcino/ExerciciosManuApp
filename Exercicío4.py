869b9b38f952ebd9c44cecf38b503b517cdb5c23
juupurcino <jdiaspurcino@gmail.com>
2024-09-13 15:08:10 -0300

    Exercicio 5 pronto

---
+++ Exercicío4.py
@@ -0,0 +1,9 @@
+h1 = int(input("Informe as horas trabalhadas no primeiro dia:"))
+h2 = int(input("Informe as horas trabalhadas no segundo dia:"))
+h3 = int(input("Informe as horas trabalhadas no terceiro dia:"))
+h4 = int(input("Informe as horas trabalhadas no quarto dia:"))
+h5 = int(input("Informe as horas trabalhadas no quinto dia:"))
+
+media = h1+h2+h3+h4+h5 / 5
+
+print(f"Média de horas trabalhadas por dia: {media}")

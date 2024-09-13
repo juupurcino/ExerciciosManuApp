727cbd793831adb41b52f00197a4fa9dad7bdc58
juupurcino <jdiaspurcino@gmail.com>
2024-09-13 16:35:22 -0300

    Exercicio 14 pronto

---
+++ Exercício14.py
@@ -0,0 +1,7 @@
+
+letra = input("Digite uma letra: ")
+
+if letra == letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
+    print("É uma vogal")
+else:
+    print("É consolante") 
